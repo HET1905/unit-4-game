85b07455765b26003df6b3d384332fc9145cb6af
Hetal Patel <nphetal1905@gmail.com>
2019-06-30 17:55:08 -0400

    completed js code and added css animate effect

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,10 +4,9 @@ var randomGem2;
 var randomGem3;
 var randomGem4;
 
-var totalScore = 0;
+// var totalScore = 0;//
 var win = 0;
 var loose = 0;
-
 
 function generateRandomNum() {
     var max = 121;
@@ -37,68 +36,65 @@ function generateRandomGem4() {
     return randomGem4;
 }
 
-var totalScore, gem1, gem2, gem3, gem4;
 $(document).ready(function () {
-    var randomNum = generateRandomNum();
-    var gem1 = generateRandomGem1();
-    var gem2 = generateRandomGem2();
-    var gem3 = generateRandomGem3();
-    var gem4 = generateRandomGem4();
+    var randomNum, gem1, gem2, gem3, gem4, totalScore;
 
-    $("#txtRandomNumber").text(randomNum);
-    $("#txtScore").text(totalScore);
+    function fnAllRandom() {
+        randomNum = generateRandomNum();
+        gem1 = generateRandomGem1();
+        gem2 = generateRandomGem2();
+        gem3 = generateRandomGem3();
+        gem4 = generateRandomGem4();
+        totalScore = 0;
+        $("#txtRandomNumber").text(randomNum);
+        $("#txtScore").text(totalScore);
+        $("#txtResult").animate({fontSize : "1em"},1500); 
+    }
+
+    fnAllRandom();
+
     $("#txtWin").text(win);
     $("#txtLoose").text(loose);
 
-    $("#gem1").click(function () {
+    function checkScore(totalScore) {
+        if (totalScore == randomNum) {
+            $("#txtResult").text("You Win!!");
+            $("#txtResult").animate({fontSize : "1.2em"},1500);
+            win++;
+            $("#txtWin").text(win);
+            
+            fnAllRandom();
+        } else if (totalScore > randomNum) {
+            $("#txtResult").text("You Loose!!");
+            $("#txtResult").animate({fontSize : "1.2em"},1500); 
+            loose++;
+            $("#txtLoose").text(loose);
+            fnAllRandom();
 
+        }
+    }
+    $("#gem1").click(function () {
         totalScore += gem1;
         $("#txtScore").text(totalScore);
-        if (totalScore == randomNum) {
-            alert("you win");
-        } else if (totalScore > randomNum) {
-            alert("you loose");
-        }
-
+        checkScore(totalScore);
     })
+
     $("#gem2").click(function () {
         totalScore += gem2;
         $("#txtScore").text(totalScore);
-        if (totalScore == randomNum) {
-            alert("you win");
-        } else if (totalScore > randomNum) {
-            alert("you loose");
-        }
+        checkScore(totalScore);
     })
 
     $("#gem3").click(function () {
         totalScore += gem3;
         $("#txtScore").text(totalScore);
-        if (totalScore == randomNum) {
-            alert("you win");
-        } else if (totalScore > randomNum) {
-            alert("you loose");
-        }
+        checkScore(totalScore);
     })
 
     $("#gem4").click(function () {
         totalScore += gem4;
         $("#txtScore").text(totalScore);
-        if (totalScore == randomNum) {
-            alert("you win");
-        } else if (totalScore > randomNum) {
-            alert("you loose");
-        }
+        checkScore(totalScore);
     })
-
-    // if(totalScore==randomNum){
-    //     alert("you win");
-    // }else if((totalScore<randomNum) || (totalScore >randomNum)) {
-    //     alert("you loose");
-    // }
-
-
-
-
 
 })
